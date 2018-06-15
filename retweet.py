443ceda4fb34d7d706d802bf5e22be77022cbe9d
Arina Ustyantseva <arina.ust@gmail.com>
2018-06-15 22:52:36 +0500

    link to tutorial

--- retweet.py
+++ retweet.py
@@ -4,6 +4,9 @@
 Created on Thu Jun 14 21:40:43 2018
 
 @author: Arina
+
+Intial version: implementing a slightly changed code from 
+https://www.digitalocean.com/community/tutorials/how-to-create-a-twitterbot-with-python-3-and-the-tweepy-library
 """
 
 import tweepy
@@ -16,8 +19,9 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-for tweet in tweepy.Cursor(api.home_timeline, count=20, page=2).items():
+for tweet in tweepy.Cursor(api.home_timeline, count=20).items():
     print(tweet)
+    print('-----------------------------')
     for keyword in keywords:    
         if keyword in tweet.text:
             try:
