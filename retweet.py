6e993f478580220fd6d0f0ce43619f1ed8fb371b
Arina Ustyantseva <arina.ust@gmail.com>
2018-06-15 22:30:56 +0500

    retweeting code

--- retweet.py
+++ retweet.py
@@ -6,3 +6,24 @@ Created on Thu Jun 14 21:40:43 2018
 @author: Arina
 """
 
+import tweepy
+from time import sleep
+from credentials import *
+from keywords import keywords
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+for tweet in tweepy.Cursor(api.home_timeline, count=20, page=2).items():
+    print(tweet)
+    for keyword in keywords:    
+        if keyword in tweet.text:
+            try:
+                tweet.retweet()
+                sleep(5)
+            except tweepy.TweepError as e:
+                print(e)
+            except StopIteration:
+                break
