1e28feb05281a40af0666b96b0e1397dfc1bc1db
Arina Ustyantseva <arina.ust@gmail.com>
2018-06-16 16:58:18 +0500

    restricted to 20 items to iterate upon

--- retweet.py
+++ retweet.py
@@ -5,29 +5,40 @@ Created on Thu Jun 14 21:40:43 2018
 
 @author: Arina
 
-Intial version: implementing a slightly changed code from 
+Twitter bot for retweeting tweets that contain specified keywords.
+
+Intial version: implementing a slightly changed code from
 https://www.digitalocean.com/community/tutorials/how-to-create-a-twitterbot-with-python-3-and-the-tweepy-library
 """
 
-import tweepy
 from time import sleep
-from credentials import *
+
+import tweepy
+from credentials import consumer_key, consumer_secret, access_token, access_token_secret
 from keywords import keywords
 
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True)
+def authorize_api():
+    """Perform user's authentication and return the API to work with."""
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+    return api
 
-for tweet in tweepy.Cursor(api.home_timeline, count=20).items():
-    print(tweet)
-    print('-----------------------------')
-    for keyword in keywords:    
-        if keyword in tweet.text:
-            try:
-                tweet.retweet()
-                sleep(5)
-            except tweepy.TweepError as e:
-                print(e)
-            except StopIteration:
-                break
+
+def retweet(api):
+    """Retweet tweets that contain keywords from the latest 20 tweets of the
+    home timeline."""
+    for tweet in tweepy.Cursor(api.home_timeline, count=20).items(20):
+        try:
+            for keyword in keywords:
+                if keyword in tweet.text:
+                    try:
+                        tweet.retweet()
+                        sleep(5)
+                    except tweepy.TweepError as exception:
+                        print(exception)
+        except StopIteration:
+            break
+
+retweet(authorize_api())
